7e91d6a6d9e2ca9a2e14c4f2f716eb375ace8b54
Vishal1654 <vchopra5@uwo.ca>
2019-03-19 08:52:31 -0400

    Adding assignment 3

---
+++ vchopra5_Assignment3/Assets/__Scripts/ShareOnTwitter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShareOnTwitter : MonoBehaviour
+{
+    public Button shareTwitter;
+    private string _twitterAddress = "http://twitter.com/intent/tweet";
+
+    void Start(){
+        shareTwitter.onClick.AddListener(OnClick);
+    }
+
+
+    void OnClick()
+    {
+        string status = "";
+        if (Scoreboard.S.score > ScoreManager.HIGH_SCORE) {
+            status = "I got a high score on Prospector Solitaire of " +Scoreboard.S.score + "!";
+        }
+        else
+        {
+            status = "I got a score of " + Scoreboard.S.score + " on Prospector Solitaire!";
+        }
+
+        Application.OpenURL(_twitterAddress +
+            "?text=" + WWW.EscapeURL(status));
+
+    }
+
+}
